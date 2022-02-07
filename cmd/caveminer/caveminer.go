7315e135eca2696db5006cab9d7f22e4309a42c1
hakbyte <hakbyte@users.noreply.github.com>
2022-02-07 07:29:33 +0200

    Parse input and open target file

--- cmd/caveminer/caveminer.go
+++ cmd/caveminer/caveminer.go
@@ -3,7 +3,10 @@ package main
 import (
 	"debug/pe"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,6 +14,32 @@ func main() {
 	if len(os.Args) != 3 {
 		Usage()
 	}
+
+	// Check if we got the correct number for cave size
+	caveSize, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatalln(err)
+	} else if caveSize <= 0 {
+		fmt.Println("Error: <cave size> must be bigger than zero")
+		Usage()
+	}
+
+	// Try to open input file or exit program otherwise
+	f, err := pe.Open(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	// Loop through all PE sections
+	sep := strings.Repeat("-", 35)
+	fmt.Println(sep)
+	for _, s := range f.Sections {
+		fmt.Printf("Section %s (%d bytes)\n", s.Name, s.Size)
+		Dig(s, caveSize)
+		fmt.Println(sep)
+	}
+
 }
 
 // Dig searches PE section for a code cave that is at least n bytes in size
